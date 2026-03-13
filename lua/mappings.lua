9f46716c5c0cb5f58bbdc8d8c91d12c0dd85cb0b
Alex Tran <alextrnnn@gmail.com>
2026-03-12 21:32:57 -0700

    better visual multi mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,7 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
@@ -11,17 +12,11 @@ vim.keymap.set("n", "<leader>ca", function()
   vim.lsp.buf.code_action()
 end, { desc = "LSP Code Action" })
 
--- vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], opts)
--- vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
--- vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
--- vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
--- vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>w]], opts)
+map("n", "<leader>y", '"+y', opts)
+map("v", "<leader>y", '"+y', opts)
 
-map("n", "<leader>y", '"+y', { noremap = true, silent = true })
-map("v", "<leader>y", '"+y', { noremap = true, silent = true })
-
-map("n", "<leader>p", '"+p', { noremap = true, silent = true })
-map("v", "<leader>p", '"+p', { noremap = true, silent = true })
+map("n", "<leader>p", '"+p', opts)
+map("v", "<leader>p", '"+p', opts)
 map("c", "<CR>", "<CR>:nohlsearch<CR>", { silent = true })
 
 vim.keymap.del("n", "<C-h>")
@@ -33,3 +28,6 @@ map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
 map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>")
 map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>")
 map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>")
+
+map("n", "<C-e>", "<Plug>(VM-Find-Under)", opts)
+map("v", "<C-e>", "<Plug>(VM-Visual-Cursors)", opts)
